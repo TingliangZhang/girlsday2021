ef7fd88f02882e40184599fe9215dcfaa99cb788
NUC <zhangtl16@mails.tsinghua.edu.cn>
2021-02-01 22:33:19 +0800

    Update NFCAntenna.py

--- NFCAntenna.py
+++ NFCAntenna.py
@@ -56,12 +56,31 @@ class AntennaWizard(FootprintWizardBase.FootprintWizard):
         # Draw Line on F.Cu layer
         self.draw.SetLayer(pcbnew.F_Cu)
         self.draw.SetLineThickness( w )
+
+        dimension = 4*n+1
+        # np.zeros((dimension, 2), dtype=int)
+
+        #  edge = [[0 for col in range(2)] for row in range(dimension)]
+        # edge = [[0] * 2] * dimension
+
+        # First loop is unique
         edge = [
-            [ a/2 , b/2 ],
-            [ -a/2 , b/2 ],
-            [ -a/2 , -b/2 ],
-            [ a/2 , -b/2 ]
+            [ -(a-w)/2 , (b-w)/2 ],
+            [ (a-w)/2 , (b-w)/2 ],
+            [ (a-w)/2 , -(b-w)/2 ],
+            [ -(a-w)/2 , -(b-w)/2 ],
+            [ -(a-w)/2 , (b-w)/2-s-w ]
                ]
+
+        for i in range(2,n+1):
+            k = i-1
+            edge[4*k][:]   = [ -(a-w)/2+(k-1)*(s+w) , (b-w)/2-k*(s+w) ]
+            edge[4*k+1][:] = [ (a-w)/2-k*(s+w) , (b-w)/2-k*(s+w) ]
+            edge[4*k+2][:] = [ (a-w)/2-k*(s+w) , -(b-w)/2+k*(s+w) ]
+            edge[4*k+3][:] = [ -(a-w)/2+k*(s+w) , -(b-w)/2+k*(s+w) ]
+        
+        edge[4*n][:] = [ -(a-w)/2+(n-1)*(s+w) , (b-w)/2-n*(s+w) ]
+
 
         # Draw edges
         self.draw.Polyline(edge)
