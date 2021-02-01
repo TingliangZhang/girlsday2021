4a38a39556df01e90522b6c03432d6f4cbdc19b6
Tingliang-Surface <ztl20@mails.tsinghua.edu.cn>
2021-02-01 12:43:00 +0800

    Update NFCAntenna.py

--- NFCAntenna.py
+++ NFCAntenna.py
@@ -9,5 +9,21 @@ class AntennaWizard(FootprintWizardBase.FootprintWizard):
     
     def GetDescription(self):
         return "NFC Antenna wizard used to draw NFC coil."
+
+    def GetValue(self):
+        return "SimpleBox_{w}x{h}mm".format(
+            w = self.parameters["Shape"]["width"],
+            h = self.parameters["Shape"]["height"])
     
+    def GenerateParameterList(self):
+        self.AddParam("Shape", "width", self.uMM, 3.75)
+        self.AddParam("Shape", "height", self.uMM, 4.95)
+
+        self.AddParam("PageB", "param3", self.uPercent, 33.05, min_value=20)
     
+    def CheckParameters(self):
+        h = self.parameters["Shape"]["height"]
+
+        self.CheckParam("Shape", "width", min_value=h/2)
+
+ExampleWizard().register()
