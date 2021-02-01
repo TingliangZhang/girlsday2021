78217f25ebde20386b70e1e81f6f8b24aba2daf0
NUC <zhangtl16@mails.tsinghua.edu.cn>
2021-02-02 07:53:31 +0800

    Finish NFC script

--- NFCAntenna.py
+++ NFCAntenna.py
@@ -9,7 +9,7 @@ class AntennaWizard(FootprintWizardBase.FootprintWizard):
         return "AntennaWizard"
     
     def GetDescription(self):
-        return "NFC Antenna wizard used to draw NFC coil."
+        return "NFC Antenna wizard used to draw NFC coil. By Tingliang Zhang"
 
     def GetValue(self):
         return "NFC-Antenna_{n}_{a}x{b}_{w}x{s}mm".format(
@@ -58,13 +58,15 @@ class AntennaWizard(FootprintWizardBase.FootprintWizard):
         self.draw.SetLineThickness( w )
 
         row = 4*n+1
+        edge = [0]*row
+        for i in range(len(edge)):
+            edge[i] = [0,0]
         # np.zeros((dimension, 2), dtype=int)
-
         #  edge = [[0 for col in range(2)] for row in range(dimension)]
         # edge = [[0] * 2] * dimension
 
         # First loop is unique
-        edge = [
+        edge[0:5] = [
             [ -(a-w)/2 , (b-w)/2 ],
             [ (a-w)/2 , (b-w)/2 ],
             [ (a-w)/2 , -(b-w)/2 ],
@@ -74,12 +76,12 @@ class AntennaWizard(FootprintWizardBase.FootprintWizard):
 
         for i in range(2,n+1):
             k = i-1
-            edge[4*k][:]   = [ -(a-w)/2+(k-1)*(s+w) , (b-w)/2-k*(s+w) ]
-            edge[4*k+1][:] = [ (a-w)/2-k*(s+w) , (b-w)/2-k*(s+w) ]
-            edge[4*k+2][:] = [ (a-w)/2-k*(s+w) , -(b-w)/2+k*(s+w) ]
-            edge[4*k+3][:] = [ -(a-w)/2+k*(s+w) , -(b-w)/2+k*(s+w) ]
+            edge[4*k]   = [ -(a-w)/2+(k-1)*(s+w) , (b-w)/2-k*(s+w) ]
+            edge[4*k+1] = [ (a-w)/2-k*(s+w) , (b-w)/2-k*(s+w) ]
+            edge[4*k+2] = [ (a-w)/2-k*(s+w) , -(b-w)/2+k*(s+w) ]
+            edge[4*k+3] = [ -(a-w)/2+k*(s+w) , -(b-w)/2+k*(s+w) ]
         
-        edge[4*n][:] = [ -(a-w)/2+(n-1)*(s+w) , (b-w)/2-n*(s+w) ]
+        edge[4*n] = [ -(a-w)/2+(n-1)*(s+w) , (b-w)/2-n*(s+w) ]
 
 
         # Draw edges
